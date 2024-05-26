6e2d78ab81b0872773b6405a8cdaaa99f32194f9
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-05-27 02:40:26 +0800

    Add Allegro initialization and error handling

--- al_game/src/core.cpp
+++ al_game/src/core.cpp
@@ -1,11 +1,20 @@
 #include "core.hpp"
 
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_ttf.h>
 #include <spdlog/logger.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
 #include <fstream>
 #include <memory>
+
+#include "error.hpp"
 
 void AlGame::Core::initialize() {
     // Slog initialization
@@ -23,5 +32,23 @@ void AlGame::Core::initialize() {
 #endif
         Slog->set_pattern("[%m-%d %H:%M:%S] %^[%l]%$ %v");
         Slog->trace("Logger initialized");
+    }
+
+    // Allegro initialization
+    {
+        if (!al_init()) throw Error("Allegro initialization failed");
+        if (!al_init_primitives_addon()) throw Error("Allegro primitives initialization failed");
+        if (!al_init_image_addon()) throw Error("Allegro image initialization failed");
+        if (!al_init_font_addon()) throw Error("Allegro font initialization failed");
+        if (!al_init_ttf_addon()) throw Error("Allegro TTF initialization failed");
+        if (!al_install_audio()) throw Error("Allegro audio initialization failed");
+        if (!al_init_acodec_addon()) throw Error("Allegro audio codec initialization failed");
+        if (!al_install_keyboard()) throw Error("Allegro keyboard initialization failed");
+        if (!al_install_mouse()) throw Error("Allegro mouse initialization failed");
+        // TODO(1plus): Solve the magic number
+        if (!al_reserve_samples(1000)) throw Error("Allegro audio sample reservation failed");
+        // Enable antialias by linear interpolation.
+        al_set_new_bitmap_flags(ALLEGRO_MIN_LINEAR | ALLEGRO_MAG_LINEAR);
+        Slog->debug("Allegro initialized");
     }
 }

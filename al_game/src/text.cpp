4a9783ccdb59aa1ab21f4f57c6e88b0a4ebcd175
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-05-27 21:20:47 +0800

    refactor: Update Text class to support custom font colors and drawing position

--- al_game/src/text.cpp
+++ al_game/src/text.cpp
@@ -1,1 +1,27 @@
 #include "text.hpp"
+
+#include <allegro5/allegro_color.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+#include "asset.hpp"
+#include "error.hpp"
+#include "vec2.hpp"
+
+using AlGame::Text;
+
+Text::Text(const char* s, int fontSize, const char* font) : str(s) {
+    this->font = Asset::getFont(font, fontSize);
+    font_color = al_map_rgb(0, 0, 0);
+    size = {al_get_text_width(this->font.get(), s), al_get_font_line_height(this->font.get())};
+}
+
+auto Text::color(int r, int g, int b) -> Text& {
+    font_color = al_map_rgb(r, g, b);
+    return *this;
+}
+
+void Text::draw(Vec2 pos, Vec2 anchor) const {
+    pos -= Vec2(size) * anchor;
+    al_draw_text(font.get(), font_color, pos.x, pos.y, 0, str.c_str());
+}
--- al_game/src/text.hpp
+++ al_game/src/text.hpp
@@ -1,14 +1,26 @@
 #ifndef TEXT_HPP
 #define TEXT_HPP
+
+#include <allegro5/allegro_font.h>
+
+#include <memory>
 #include <string>
-#include <utility>
+
+#include "vec2.hpp"
+
 namespace AlGame {
 class Text {
+    std::shared_ptr<ALLEGRO_FONT> font;
+    ALLEGRO_COLOR font_color;
+    // content and its size
     std::string str;
+    Vec2Int size;
 
    public:
-    explicit Text(const char* s) : str(s) {};
-    explicit Text(std::string s) : str(std::move(s)) {};
+    explicit Text(const char* s, int fontSize, const char* font = "pirulen.ttf");
+
+    auto color(int r, int g, int b) -> Text&;
+    void draw(Vec2 pos, Vec2 anchor = {0.5, 0.5}) const;
 };
 }  // namespace AlGame
 #endif  // TEXT_HPP

be73bf4fda236fe37df34a086d662fcf5b1eb9e7
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-05-27 13:07:59 +0800

    fix build error

--- al_game/src/error.hpp
+++ al_game/src/error.hpp
@@ -1,11 +1,17 @@
 #ifndef ERROR_HPP
 #define ERROR_HPP
+
 #include <stdexcept>
+
+#include "log.hpp"
+
 namespace AlGame {
 class Error final : std::runtime_error {
    public:
-    explicit Error(const char* msg) noexcept;
-    explicit Error(const std::string& msg) noexcept;
+    explicit Error(const char* msg) noexcept : std::runtime_error(msg) {
+        AlGame::Log::Slog->critical(msg);
+    }
+    explicit Error(const std::string& msg) noexcept : Error(msg.c_str()) {};
 };
 }  // namespace AlGame
 #endif  // ERROR_HPP

83e4a1975951f1d6b0f321bed7cd1d30e7f76645
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-05-27 18:47:08 +0800

    feat: Add contains() method to Rect class

--- al_game/include/rect.hpp
+++ al_game/include/rect.hpp
@@ -7,6 +7,11 @@ struct Rect {
 
     constexpr Rect(Vec2 pos, Vec2 size) : pos(pos), size(size) {}
 
+    [[nodiscard]] constexpr auto contains(Vec2 point) const -> bool {
+        return point.x >= pos.x && point.x <= pos.x + size.x && point.y >= pos.y &&
+               point.y <= pos.y + size.y;
+    }
+
     [[nodiscard]] constexpr auto overlaps(const Rect& rect) const -> bool {
         return pos.x < rect.pos.x + rect.size.x && pos.x + size.x > rect.pos.x &&
                pos.y < rect.pos.y + rect.size.y && pos.y + size.y > rect.pos.y;
